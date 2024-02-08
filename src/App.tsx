8fc6b4adf64e12c61aad7d624d14192efd96fd2f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-09 05:28:48 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
+import Contact from "./contact/Contact"
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Contact />    
   )
 }
