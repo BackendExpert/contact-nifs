a7a2c184850f9cea870e36c5a16ec087c4186840
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-09 05:32:06 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/contact/Contact.tsx
+++ src/contact/Contact.tsx
@@ -5,7 +5,15 @@ const Contact = () => {
       <h1 className="">Contact</h1>
       <p className="">Office Location</p>
       <div className="md:grid grid-cols-3 gap-4">
-
+        <div className="">
+          <p className="">Headquarters</p>
+        </div>
+        <div className="">
+          <p className="">Colombo Office</p>
+        </div>
+        <div className="">
+          <p className="">NIFS Sam Popham's Arboretum</p>
+        </div>
       </div>
     </div>
   )
