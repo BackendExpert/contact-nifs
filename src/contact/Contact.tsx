fa691640b35ed63330f5ae88f1178632bf8e7c15
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-09 05:34:03 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/contact/Contact.tsx
+++ src/contact/Contact.tsx
@@ -7,12 +7,30 @@ const Contact = () => {
       <div className="md:grid grid-cols-3 gap-4">
         <div className="">
           <p className="">Headquarters</p>
+          <div className="">
+            <p>National Institute of Fundamental Studies</p>
+            <p>Hanthana Road</p>
+            <p>Kandy</p>
+            <p>Sri Lanka</p>
+          </div>
         </div>
         <div className="">
           <p className="">Colombo Office</p>
+          <div className="">
+            <p>National Institute of Fundamental Studies</p>
+            <p>Hanthana Road</p>
+            <p>Kandy</p>
+            <p>Sri Lanka</p>
+          </div>
         </div>
         <div className="">
           <p className="">NIFS Sam Popham's Arboretum</p>
+          <div className="">
+            <p>National Institute of Fundamental Studies</p>
+            <p>Hanthana Road</p>
+            <p>Kandy</p>
+            <p>Sri Lanka</p>
+          </div>
         </div>
       </div>
     </div>
