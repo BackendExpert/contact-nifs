d44187d46d8cd578ccd6245d3ce2a8a73a61974b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-09 05:30:51 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/contact/Contact.tsx
+++ src/contact/Contact.tsx
@@ -2,7 +2,11 @@
 const Contact = () => {
   return (
     <div className="container mx-auto px-10">
-      Hi all
+      <h1 className="">Contact</h1>
+      <p className="">Office Location</p>
+      <div className="md:grid grid-cols-3 gap-4">
+
+      </div>
     </div>
   )
 }
